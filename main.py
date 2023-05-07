2f257c24a3dc05a96770add03e078687a0cde0cd
Kanishk Malhotra <78301665+KanishkM08@users.noreply.github.com>
2023-05-07 16:31:04 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,55 @@
+ch = int(input("Multiplying two (1.)square matrices or (2.)rectangular matrices? (1/2): "))
+
+if ch == 1:
+    # get the dimensions of square matrices
+    n = int(input("Enter square matrices order(nxn): "))
+    r = n
+    c = n
+    r2 = n
+    c2 = n
+elif ch == 2:
+    r = int(input("Enter no. of rows of first matrix: "))
+    c = int(input("Enter no. of columns of first matrix:"))
+    r2 = int(input("Enter no. of rows of second matrix: "))
+    c2 = int(input("Enter no. of columns of second matrix:"))
+    if c!=r2:
+        print("The given confirguration is not valid to multiply two matrices")
+        exit()
+else:
+    print("Please enter valid number")
+
+# get the elements of the first matrix
+print("Enter the elements of the first matrix:")
+matrix1 = [[int(input()) for x in range(c)] for y in range(r)]
+# Print the first matrix
+print("The first matrix entered:")
+for row in matrix1:
+    print(row)
+print("order: ",r,"x",c)
+
+# get the elements of the second matrix
+print("Enter the elements of the second matrix:")
+matrix2 = [[int(input()) for x in range(c2)] for y in range(r2)]
+# Print the second matrix
+print("The second matrix entered:")
+for row in matrix2:
+    print(row)
+print("order: ",r2,"x",c2)
+
+# create a new matrix to store the result
+matrix = []
+
+# Multiplying and adding
+for i in range(r):
+        row = []
+        for j in range(c2):
+            sum = 0
+            for k in range(c):
+                sum += matrix1[i][k] * matrix2[k][j]
+            row.append(sum)
+        matrix.append(row)
+            
+# Print the result matrix
+print("The result of the multiplication is:")
+for row in matrix:
+    print(row)
